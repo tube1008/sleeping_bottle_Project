57ce8afa1b418d17f259b1235cfae4ed3c575daf
tube1008 <ilhyuk0504@gmail.com>
2024-02-14 20:47:02 +0900

    팝업창창창

--- script/index.js
+++ script/index.js
@@ -21,9 +21,13 @@ const searchBox = document.querySelector ('.searchBox')//아이디 비밀번호 
 const bottom = document.querySelector ('.bottom')// 로그인 팝업창 밑 비회원 배송조회 부분
 const nonShipping = document.querySelector ('.nonShipping') //비회원 배송조회 버튼
 const nondelivery = document.querySelector ('.nondelivery') // 비회원 배송조회 팝업창
+const nonlogin = document.querySelector ('.nonlogin') //비회원 배송조회에서 로그인하기
+const terms = document.querySelector ('.terms') // 약관동의
+const joinMembership = document.querySelector ('.joinMembership') // 약관동의로 넘어가기
+const signupBtn = document.querySelector ('.signupBtn') // 가입하기로 들어가기 버튼
+const signContainer =document.querySelector ('.signContainer') //가입하기 팝업창
 
-
-console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonShipping, nondelivery)
+console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonShipping, nondelivery, nonlogin, terms, joinMembership, signupBtn, signContainer)
 
 loginpopup_bg.style.display = 'none'
 joinpopup_bg.style.display = 'none'
@@ -44,11 +48,15 @@ idPwfind.addEventListener('click', function(){
     nonShipping.style.display = 'none'
 })
 
-nonShipping.addEventListener('click', function(e){
-    console.log('5646')
-    e.preventDefault()
+nonShipping.addEventListener('click', function(){
     nondelivery.style.display = 'block'
     login_access.classList.add('hide')
+    loginpopup_bg.style.display = 'flex'
+})
+
+nonlogin.addEventListener('click', function(){
+    nondelivery.style.display = 'none'
+    login_access.classList.remove('hide')
     loginpopup_bg.style.display = 'flex'
 })
 
@@ -69,8 +77,17 @@ join.addEventListener('click', function(){
     join_access.style.display = 'block'
 })
 
+joinMembership.addEventListener('click', function(){
+    joinpopup_bg.style.display = 'flex'
+    terms.style.display = 'block'
+    join_access.style.display = 'none'
+})
 
-
+signupBtn.addEventListener('click',function(){
+    joinpopup_bg.style.display = 'flex'
+    terms.style.display = 'none'
+    signContainer.style.display = 'block'
+})
 
 
 // let slide = new Swiper('.swiper-container',{
