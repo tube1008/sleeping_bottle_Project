5ad4156effc44175b1c31b8b3dde28a721b80121
Ahn il hyuk <ilhyuk0504@gmail.com>
2024-02-14 18:29:56 +0900

    나머지팝업하자

--- script/index.js
+++ script/index.js
@@ -19,16 +19,15 @@ const joinGo = document.querySelector ('.joinGo')// 회원가입창으로 가기
 const idPwfind = document.querySelector ('.idPwfind')//아이디,비밀번호 찾기
 const searchBox = document.querySelector ('.searchBox')//아이디 비밀번호 팝업창
 const bottom = document.querySelector ('.bottom')// 로그인 팝업창 밑 비회원 배송조회 부분
-const nonshipping = document.querySelector ('.nonShipping') //비회원 배송조회 버튼
+const nonShipping = document.querySelector ('.nonShipping') //비회원 배송조회 버튼
 const nondelivery = document.querySelector ('.nondelivery') // 비회원 배송조회 팝업창
 
 
-console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonshipping, nondelivery)
+console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonShipping, nondelivery)
 
 loginpopup_bg.style.display = 'none'
 joinpopup_bg.style.display = 'none'
 searchBox.style.display = 'none'
-nondelivery.style.display = 'none'
 login_access.style.display = 'none'
 join_access.style.display = 'none'
 
@@ -42,12 +41,15 @@ idPwfind.addEventListener('click', function(){
     login_access.style.display = 'none'
     loginpopup_bg.style.display = 'flex'
     bottom.style.display = 'none'
-    nonshipping.style.display = 'none'
+    nonShipping.style.display = 'none'
 })
-nonshipping.addEventListener('click', function(){
+
+nonShipping.addEventListener('click', function(e){
+    console.log('5646')
+    e.preventDefault()
     nondelivery.style.display = 'block'
+    login_access.classList.add('hide')
     loginpopup_bg.style.display = 'flex'
-    login_access.style.display = 'none'
 })
 
 loginpopup_bg.addEventListener('click', function() {
@@ -62,7 +64,7 @@ joinGo.addEventListener('click', function(){
     login_access.style.display = 'block'
 })
 
-join.addEventListener('click',function(){
+join.addEventListener('click', function(){
     joinpopup_bg.style.display = 'flex'
     join_access.style.display = 'block'
 })
