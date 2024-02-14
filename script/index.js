6368cc164fb23ad10951e832b00518cbe45cf7c8
tube1008 <ilhyuk0504@gmail.com>
2024-02-15 02:13:33 +0900

    아이디비번팝업

--- script/index.js
+++ script/index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// 로그인, 회원가입, 아이디/비번찾기, 비회원 배송조회, 약관동의 팝업창
 const body = document.querySelector('body, html')
 const loginpopup_bg = document.querySelector ('.loginPop')//로그인 배경
 const joinpopup_bg = document.querySelector ('.joinWrap')// 회원가입 배경
@@ -26,14 +27,20 @@ const terms = document.querySelector ('.terms') // 약관동의
 const joinMembership = document.querySelector ('.joinMembership') // 약관동의로 넘어가기
 const signupBtn = document.querySelector ('.signupBtn') // 가입하기로 들어가기 버튼
 const signContainer =document.querySelector ('.signContainer') //가입하기 팝업창
+const term = document.querySelector ('.term') // footer 배경
+const useTerms = document.querySelector ('.useTerms') // footer 이용약관 버튼
+const utiliTitle = document.querySelector ('.utiliTitle') // footer 이용약관
+const infoPolicy = document.querySelector ('.infoPolicy') // footer 개인정보처리방침 버튼
+const policyTitle = document.querySelector ('.policyTitle') // footer 개인정보처리방침 
 
-console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonShipping, nondelivery, nonlogin, terms, joinMembership, signupBtn, signContainer)
+console.log(login, loginpopup_bg, body, login_access, join, join_access, joinpopup_bg, joinGo, idPwfind, searchBox, bottom, nonShipping, nondelivery, nonlogin, terms, joinMembership, signupBtn, signContainer, useTerms, infoPolicy, term, utiliTitle, policyTitle )
 
 loginpopup_bg.style.display = 'none'
 joinpopup_bg.style.display = 'none'
 searchBox.style.display = 'none'
 login_access.style.display = 'none'
 join_access.style.display = 'none'
+term.style.display = 'none'
 
 login.addEventListener('click',function(){
     loginpopup_bg.style.display = 'flex'
@@ -42,7 +49,7 @@ login.addEventListener('click',function(){
 
 idPwfind.addEventListener('click', function(){
     searchBox.style.display = 'block'
-    login_access.style.display = 'none'
+    login_access.classList.add('hide')
     loginpopup_bg.style.display = 'flex'
     bottom.style.display = 'none'
     nonShipping.style.display = 'none'
@@ -89,11 +96,31 @@ signupBtn.addEventListener('click',function(){
     signContainer.style.display = 'block'
 })
 
+// footer area
 
-// let slide = new Swiper('.swiper-container',{
-//     direction: 'horizontal',
-//     loop:'true',
-//     slidesPerView:3,
-//     spaceBetween:10,
-//     autoplay:{delay:5000,}
-// })
+useTerms.addEventListener('click', function(){
+    term.style.display = 'flex'
+    utiliTitle.style.display = 'block'
+})
+
+infoPolicy.addEventListener('click', function(){
+    term.style.display = 'flex'
+    policyTitle.style.display = 'block'
+})
+
+const searchIdd = document.querySelector ('.searchIdd') // 아이디 찾기
+const emailId = document.querySelector ('#emailId') // 이메일로 찾기
+const searchPww = document.querySelector ('.searchPww') // 비밀번호 찾기
+const username = document.querySelector ('#username') // 이름으로 찾기
+const phNumber = document.querySelector ('#phNumber') // 번호로 찾기
+const selectBoxNum = document.querySelector ('#selectBoxNum') //체크박스 지워야함
+const selectBoxMail = document.querySelector ('#selectBoxMail .mail')
+console.log (searchIdd, searchPww, emailId, username, phNumber, selectBoxMail )
+
+emailId.style.display = 'none'
+username.style.display = 'none'
+phNumber.style.display = 'none'
+selectBoxNum.style.display = 'none'
+
+
+
